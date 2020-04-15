adf083b1aae206c480f62231b7d8e9d824ae095f
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-04-14 19:36:41 -0500

    fixed the validateUser, added two more endpoints

--- users/userRouter.js
+++ users/userRouter.js
@@ -3,24 +3,52 @@ const users = require("./userDb");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
-    // do your magic!
+router.post("/", validateUser(), (req, res) => {
+    users
+        .insert(req.body)
+        .then((user) => {
+            res.status(201).json(user);
+        })
+        .catch((err) => {
+            next(err);
+        });
 });
 
-router.post("/:id/posts", (req, res) => {
-    // do your magic!
+router.post("/:id/posts", validateUserId(), (req, res) => {
+    users
+        .insert(req.body)
+        .then((post) => {
+            res.status(201).json(post);
+        })
+        .catch((err) => {
+            next(err);
+        });
 });
 
 router.get("/", (req, res) => {
-    // do your magic!
+    users
+        .get()
+        .then((users) => {
+            res.status(200).json(users);
+        })
+        .catch((err) => {
+            next(err);
+        });
 });
 
-router.get("/:id", (req, res) => {
-    // do your magic!
+router.get("/:id", validateUserId(), (req, res) => {
+    res.status(200).json(req.user);
 });
 
-router.get("/:id/posts", (req, res) => {
-    // do your magic!
+router.get("/:id/posts", validateUserId(), (req, res) => {
+    users
+        .getUserPosts(req.params.id)
+        .then((posts) => {
+            res.status(200).json(posts);
+        })
+        .catch((err) => {
+            next(err);
+        });
 });
 
 router.delete("/:id", (req, res) => {
@@ -33,30 +61,54 @@ router.put("/:id", (req, res) => {
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-    users
-        .getById(req.params.id)
-        .then((user) => {
-            if (user) {
-                req.user = user;
-                next();
-            } else {
-                res.status(400).json({
-                    message: "User not found",
-                });
-            }
-        })
-        .catch((err) => {
-            next(err);
-        });
+function validateUserId() {
+    return (req, res, next) => {
+        users
+            .getById(req.params.id)
+            .then((user) => {
+                if (user) {
+                    req.user = user;
+                    next();
+                } else {
+                    res.status(400).json({
+                        message: "invalid user id",
+                    });
+                }
+            })
+            .catch((err) => {
+                next(err);
+            });
+    };
 }
 
-function validateUser(req, res, next) {
-    // do your magic!
+function validateUser() {
+    return (req, res, next) => {
+        if (!req.body) {
+            return res.status(400).json({
+                message: "missing user data",
+            });
+        } else if (!req.body.name) {
+            return res.status(400).json({
+                message: "missing required name field",
+            });
+        }
+        next();
+    };
 }
 
-function validatePost(req, res, next) {
-    // do your magic!
+function validatePost() {
+    return (req, res, next) => {
+        if (!req.body) {
+            return res.status(400).json({
+                message: "missing post data",
+            });
+        } else if (!req.body.text) {
+            return res.status(400).json({
+                message: "missing required text field",
+            });
+        }
+        next();
+    };
 }
 
 module.exports = router;
